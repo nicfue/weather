eb6fb5689f9603cd9820d11975e66403df7de361
Nicolas Fuentes <nicolasfuentes@hotmail.com>
2017-03-26 03:25:06 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var api_key = '9464c10b789eff56ceff53e0cc4436fe';
+
+// URL:er
+url_weather = 'http://api.openweathermap.org/data/2.5/weather?q=';
+url_forecast = 'http://api.openweathermap.org/data/2.5/forecast?q=';
+url_sunrise_sunset = 'http://api.sunrise-sunset.org/json?lat='
+
 $(document).ready(function() {
 
   // Väder
@@ -7,12 +14,12 @@ $(document).ready(function() {
       if (city != '') {
 
         $.ajax({
-
-          url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric' + '&APPID=9464c10b789eff56ceff53e0cc4436fe',
+          url: url_weather + city + '&units=metric' + '&APPID=' + api_key,
           type: 'GET',
           dataType: 'jsonp',
           success: function(data) {
               var result = show(data);
+              console.log(data);
 
               $('#show').html(result);
               $('#city').val('');
@@ -25,18 +32,28 @@ $(document).ready(function() {
   });
 
 function show(data) {
+
   console.log(data);
-    return '<h4>Aktuellt väder för '+ data.name +', '+ data.sys.country +'</h4>' +
-           '<h5>Väder: '+ data.weather[0].main + '</5>' +
-           '<h5>Beskrivning: <img src="http://openweathermap.org/img/w/'+ data.weather[0].icon +'.png"> '+ data.weather[0].description +'</5>' +
-           '<h5>Moln: '+ data.clouds.all +'%</5>' +
-           '<h5>Temperatur: '+ data.main.temp +'&deg;C</5>' +
-           '<h5>Lufttryck: '+ data.main.pressure +' hpa</5>' +
-           '<h5>Luftfuktighet: '+ data.main.humidity +'%</5>' +
-           '<h5>Min temp: '+ data.main.temp_min +'&deg;C</5>' +
-           '<h5>Max temp: '+ data.main.temp_max +'&deg;C</5>' +
-           '<h5>Vindhastighet: '+ data.wind.speed +' m/s</5>' +
-           '<h5>Vindriktning: '+ data.wind.deg +'&deg;</5>';
+  var sunrise = data.sys.sunrise;
+  var sunset = data.sys.sunset;
+  var format = "LT";
+  var sunrise_convert = moment.unix(sunrise).format(format);
+  var sunset_convert = moment.unix(sunset).format(format);
+
+    return '<h4>Väder i '+ data.name +', '+ data.sys.country +'</h4>' +
+    '<h2><img src="http://openweathermap.org/img/w/'+ data.weather[0].icon +'.png"> '+ data.main.temp + '&deg;C</h2>' +
+           '<h6>'+ data.weather[0].main + '</h6>' +
+           '<h5>Moln: '+ data.clouds.all +'%</h5>' +
+           '<h5>Temperatur: '+ data.main.temp +'&deg;C</h5>' +
+           '<h5>Lufttryck: '+ data.main.pressure +' hpa</h5>' +
+           '<h5>Luftfuktighet: '+ data.main.humidity +'%</h5>' +
+           '<h5>Min temp: '+ data.main.temp_min +'&deg;C</h5>' +
+           '<h5>Max temp: '+ data.main.temp_max +'&deg;C</h5>' +
+           '<h5>Vindhastighet: '+ data.wind.speed +' m/s</h5>' +
+           '<h5>Vindriktning: '+ data.wind.deg +'&deg;</h5>'+
+           '<h5>Soluppgång: '+ sunrise_convert +' </h5>' +
+           '<h5>Solnedgång: '+ sunset_convert +'</h5>';
+
 }
 
 
@@ -47,8 +64,7 @@ function show(data) {
       if (city != '') {
 
         $.ajax({
-
-          url: 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=metric' + '&APPID=9464c10b789eff56ceff53e0cc4436fe',
+          url: url_forecast + city + '&units=metric' + '&APPID=' + api_key,
           type: 'GET',
           dataType: 'jsonp',
           success: function(forecast) {
@@ -79,7 +95,42 @@ $('#show2').html(html)
 }
 
 
+// sunrise & sunset
+$(function() {
+  var loc;
+
+  $.getJSON('http://ipinfo.io/', function(coord){
+    console.log(coord);
+    loc = coord.loc.split(",");
+    console.log(loc);
+
+    $.ajax({
+      url: url_sunrise_sunset + loc[0] + '&lng=' + loc[1],
+      type: "GET",
+      dataType: 'jsonp',
+      success: function(data) {
+        console.log(data);
+        let sunrise  = showSunrise(data)
+        $('#display1').html(sunrise);
+        let sunset = showSunset(data)
+        $('#display2').html(sunset);
+      }
+
+    })
+  })
+function showSunrise(data){
+
+  return '<h1>Soluppgång: '+ data.results.sunrise +' <br> <img src="app/pictures/sunrise.png" width="470" height="300" alt="sunrise"</1>';
+}
+
+function showSunset(data) {
+  return '<h1 class="sunset"> Solnegång: '+ data.results.sunset +' <br> <img src="app/pictures/sunset.png" width="450" height="320" alt="sunrise"</h1>';
+  }
+ })
+
 });
+
+
 // reset text
 $('input:text').focus(
   function(){
