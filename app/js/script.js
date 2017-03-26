76a547c501e6f09ae6f19d725b3be5508d5cb357
nicfue <nicolasfuentes@hotmail.com>
2017-03-27 01:52:59 +0200

    Börjat kommentera ut koden

--- app/js/script.js
+++ app/js/script.js
@@ -5,33 +5,40 @@ url_weather = 'http://api.openweathermap.org/data/2.5/weather?q=';
 url_forecast = 'http://api.openweathermap.org/data/2.5/forecast?q=';
 url_sunrise_sunset = 'http://api.sunrise-sunset.org/json?lat='
 
+// Vi förbereder för domen
 $(document).ready(function() {
 
   // Väder
+
+  // Använder selectorn för att lägga till ett click event till knappen för väder, hämtar input från #submitWeather
   $('#submitWeather').click(function() {
-
+// Selectorn används för att hämta värdet från inmatningsfältet med id cityWeather
       var city = $('#cityWeather').val();
+      // Om ingenting matas i inmatningsfältet med id "cityWeather" visas ett felmeddelande
       if (city != '') {
-
+        // Vi använder ajax metoden för att hämta datan via url, type och dataType. För att få ut värdena i Celcius lägger vi till units=metric.
         $.ajax({
           url: url_weather + city + '&units=metric' + '&APPID=' + api_key,
           type: 'GET',
           dataType: 'jsonp',
+          // JSON datan från vår request sparas i funktionen "success" som vi sedan skickar vidare till funktionen "showWeather" för att visa upp innehållet i domen.
           success: function(data) {
-              var result = show(data);
+              var result = showWeather(data);
               console.log(data);
-
-              $('#show').html(result);
-              $('#city').val('');
+              // Vi använder selectorn för att visa innehållet i diven med "id=weather"
+              $('#weather').html(result);
+              // Inmatningsfältet nollställs efter varje inmatning.
+              $('#cityWeather').val('');
           }
         });
+        // Felmeddelande som visas om ingenting matas in i inmatningsfältet för Väder
       }else {
         $('#errorWeather').html("<div class='alert alert-danger'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>Fältet får ej vara tomt</div>");
 
       }
   });
-
-function show(data) {
+// Funktionen "showWeather" skapas för att visa upp innehållet från JSON datan via parametern "data".
+function showWeather(data) {
 
   console.log(data);
   var sunrise = data.sys.sunrise;
@@ -40,12 +47,14 @@ function show(data) {
   var sunrise_convert = moment.unix(sunrise).format(format);
   var sunset_convert = moment.unix(sunset).format(format);
 
-    return '<h4>Väder i '+ data.name +', '+ data.sys.country +'</h4>' +
+
+// Här returneras all den data vi vill visa upp i HTML. Med plustecknet kan vi lägga till alla parametrar vi valt ut för display.
+    return '<h3>Väder i '+ data.name +', '+ data.sys.country +'</h3>' +
     '<h2><img src="http://openweathermap.org/img/w/'+ data.weather[0].icon +'.png"> '+ data.main.temp + '&deg;C</h2>' +
-           '<h6>'+ data.weather[0].main + '</h6>' +
+           '<h6>'+ data.weather[0].description + '</h6> <br>' +
            '<h5>Moln: '+ data.clouds.all +'%</h5>' +
            '<h5>Temperatur: '+ data.main.temp +'&deg;C</h5>' +
-           '<h5>Lufttryck: '+ data.main.pressure +' hpa</h5>' +
+           '<h5>Lufttryck: '+ data.main.pressure +' hPa</h5>' +
            '<h5>Luftfuktighet: '+ data.main.humidity +'%</h5>' +
            '<h5>Min temp: '+ data.main.temp_min +'&deg;C</h5>' +
            '<h5>Max temp: '+ data.main.temp_max +'&deg;C</h5>' +
@@ -58,40 +67,48 @@ function show(data) {
 
 
 // Prognos
+
+// Använder selectorn för att lägga till ett click event till knappen för Väderprognos, hämtar input från #submitForecast
   $('#submitForecast').click(function() {
-
+    // Selectorn används för att hämta värdet från inmatningsfältet med id cityForecast
       var city = $('#cityForecast').val();
+      // Om ingenting matas i inmatningsfältet med id "cityForecast" visas ett felmeddelande
       if (city != '') {
-
+        // Vi använder ajax metoden för att hämta datan via url, type och dataType. För att få ut värdena i Celcius lägger vi till units=metric.
         $.ajax({
           url: url_forecast + city + '&units=metric' + '&APPID=' + api_key,
           type: 'GET',
           dataType: 'jsonp',
-          success: function(forecast) {
-              var result = show2(forecast);
-
-              $('#show2').html(result);
-              $('#city').val('');
+          // JSON datan från vår request sparas i funktionen "success" som vi sedan skickar vidare till funktionen "showForecast" för att visa upp innehållet i domen.
+          success: function(data) {
+              var result = showForecast(data);
+              // Vi använder selectorn för att visa innehållet i diven med "id=forecast"
+              $('#forecast').html(result);
+              // Inmatningsfältet nollställs efter varje inmatning.
+              $('#cityForecast').val('');
           }
         });
+
+        // Felmeddelande som visas om ingenting matas in i inmatningsfältet för Väderprognos
       }else {
         $('#errorForecast').html("<div class='alert alert-danger'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>Fältet får ej vara tomt</div>");
 
       }
   });
 
-function show2(forecast) {
-  console.log(forecast);
+  // Funktionen "showForecast" skapas för att visa upp innehållet från JSON datan via parametern "data".
+function showForecast(data) {
+  console.log(data);
   var html = '',
-  cityName = forecast.city.name,
-  country = forecast.city.country
+  cityName = data.city.name,
+  country = data.city.country
 
-html += '<h4>5-dagars prognos med 3-timmars intevaller ' + cityName +', '+ country +'</h4>'
+html += '<h3>5-dagars prognos med 3-timmars intevaller ' + cityName +', '+ country +'</h3>'
 
-forecast.list.forEach(function(forecastEntry, index, list) {
-  html += '<h5 class="table-active stripe">'+forecastEntry.dt_txt +': '+forecastEntry.main.temp +' &deg;C </h5>'
+data.list.forEach(function(currentValue, index, list) {
+  html += '<h5 class="table-active stripe">'+ currentValue.dt_txt +': '+ currentValue.main.temp +' &deg;C </h5>'
 })
-$('#show2').html(html)
+$('#forecast').html(html)
 }
 
 
